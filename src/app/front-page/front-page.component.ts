a2022a26fcc200612c3f3e8d8e2d96ad4a5e1df8
Shakira Dale <daleshakira91@gmail.com>
2018-04-24 17:12:37 -0400

    things

--- src/app/front-page/front-page.component.ts
+++ src/app/front-page/front-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-front-page',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./front-page.component.css']
 })
 export class FrontPageComponent implements OnInit {
-
-  constructor() { }
+  wp_content: any;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('https://cors-anywhere.herokuapp.com/http://dev-hias-wordpress-testing.pantheonsite.io/wp-json/custom/v1/front-page').subscribe((data) => {
+      this.http.get('http://dev-hias-wordpress-testing.pantheonsite.io/wp-json/wp/v2/pages/'+data).subscribe((data) => {
+        this.wp_content = data;
+      });
+     });
   }
 
 }

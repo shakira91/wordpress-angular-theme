6b3c55da234da91e7af60866d797bf5e0b4a0be0
Shakira Dale <daleshakira91@gmail.com>
2018-04-26 16:53:32 -0400

    nav

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -10,20 +10,29 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 export class NavigationComponent implements OnInit {
   @Input() frontPageContentLodaded; //if the content on the front page has loaded, load navigation
   navItems: any;
-  
+  navItemsArray = [];
+
   constructor(private http: HttpClient, private renderer: Renderer2, private el: ElementRef) { }
 
-  navItemHovered(navItem, event) {
-    if(navItem.menu_item_parent > 0) {
-      this.renderer.addClass(event.target, 'sub-menu')
-    }
+  // navItemHovered(navItem, event) {
+  //   if(navItem.menu_item_parent > 0) {
+  //     this.renderer.addClass(event.target, 'sub-menu')
+  //   }
     
-  }
+  // }
 
   ngOnInit() {
     this.http.get('https://cors-anywhere.herokuapp.com/http://dev-hias-wordpress-testing.pantheonsite.io/wp-json/custom/v1/menus').subscribe((data) => {
      this.navItems = data;
+      this.navItems.forEach((element, index) => {
+      const navItemsObj = {};
+         navItemsObj['navID'] = element.ID;
+         navItemsObj['navTitle'] = element.title;
+         navItemsObj['navParentID'] = element.menu_item_parent;
+         this.navItemsArray.push(navItemsObj)
+      });  
     });
+    console.log(this.navItemsArray)
   }
 
 }

716dafacae2afdad8ed0fcaf7dab98ea8a3ffacb
Shakira D <4237135+shakira91@users.noreply.github.com>
2018-08-01 21:08:08 -0400

    work sizes

--- src/app/process/process.component.ts
+++ src/app/process/process.component.ts
@@ -11,9 +11,11 @@ export class ProcessComponent implements OnInit, AfterViewInit {
     'http://dev.etherealcreative.com/wp-content/uploads/2018/07/49-chambers_waterfront_lifestyle-14.jpg',
     'http://dev.etherealcreative.com/wp-content/uploads/2018/07/0N9A3046.jpg',
     'http://dev.etherealcreative.com/wp-content/uploads/2018/07/TRIBECAREDMOPED.jpg',
-    'http://dev.etherealcreative.com/wp-content/uploads/2018/07/TRIBECAREDMOPED.jpg'
+    'http://dev.etherealcreative.com/wp-content/uploads/2018/07/49-chambers_waterfront_lifestyle-14.jpg'
   ];
-
+  defaultImage = 'https://images.unsplash.com/photo-1468413922365-e3766a17da9e?dpr=2&auto=compress,format&fit=crop&w=1199&h=800&q=80';
+  backgroundImage = 'https://images.unsplash.com/photo-1443890923422-7819ed4101c0?fm=jpg';
+  offset = 100;
   constructor() { }
 
   ngAfterViewInit() {
